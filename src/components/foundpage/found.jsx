08dda52c7ec4227711babd97c3abf6b4ecf20324
Daithoulung <daithoulungrongmai@gmail.com>
2024-10-01 00:10:26 +0530

    update lost/found page

--- src/components/foundpage/found.jsx
+++ src/components/foundpage/found.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import './style.css'
 const found = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="FoundTitle">
+        Add Found ‎‎ <span>Item</span>
+      </div>
+      <div className="Img"></div>
+      <div className="PSelect"></div>
+      <div className="IName">
+        <label for="IName">Item Name</label>
+        <input type="text" id="IName" className="NameField"></input>
+      </div>
+      <div className="IFound">
+        <label for="ILocation">Found At</label>
+        <input type="text" id="ILocation" className="FoundField"></input>
+      </div>
+      <div className="PhoneNumber">
+        <label for="INumber">Phone Number</label>
+        <input type="number" id="INumber" className="NumberField"></input>
+      </div>
+      <textarea className="description" id="IDescription" placeholder="Description"></textarea>
+      <button className='PostBtn'>Post</button>
+    </>
   )
 }
 
--- src/components/lostpage/Lost.jsx
+++ src/components/lostpage/Lost.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import "./style.css"
 const Lost = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="lostTitle">
+        Add Lost ‎‎ <span>Item</span>
+      </div>
+      <div className="Img"></div>
+      <div className="PSelect"></div>
+      <div className="IName">
+        <label for="IName">Item Name</label>
+        <input type="text" id="IName" className="NameField"></input>
+      </div>
+      <div className="ILost">
+        <label for="ILocation">Lost At</label>
+        <input type="text" id="ILocation" className="LostField"></input>
+      </div>
+      <div className="PhoneNumber">
+        <label for="INumber">Phone Number</label>
+        <input type="number" id="INumber" className="NumberField"></input>
+      </div>
+      <textarea className="description" id="IDescription" placeholder="Description"></textarea>
+      <button className='PostBtn'>Post</button>
+    </>
   )
 }
 
