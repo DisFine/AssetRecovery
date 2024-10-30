6e7b748e3e8f31ddf4b2c6163d7b70c1119568f3
Daithoulung <daithoulungrongmai@gmail.com>
2024-10-31 02:13:34 +0530

    updated design

--- src/components/createClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://dzztvvyzqiuksuyyrfzy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6enR2dnl6cWl1a3N1eXlyZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2MzMxMzcsImV4cCI6MjA0MzIwOTEzN30.Y77DWzAIwnLQC-GfqfwNAu83v37cikLmRH_x8NGdmJk'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
