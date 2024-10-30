3b2f39e32359f0137d7cacc0ff6f9bf69d2bdbb0
Daithoulung <daithoulungrongmai@gmail.com>
2024-10-31 03:32:13 +0530

    updated design

--- src/components/foundpage/FoundPage.jsx
+++ src/components/foundpage/FoundPage.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { useState } from "react";
 import "../style.css";
 function FoundPage({ supabase }) {
+  const [selectedIcon, setSelectedIcon] = useState('camera');
+
+  const handleSelect = (icon) => {
+    setSelectedIcon(icon);
+  };
+
   return (
     <>
       <div className="outerContainer Adjustment" >
@@ -8,7 +14,16 @@ function FoundPage({ supabase }) {
           Add Found ‎‎ <span>Item</span>
         </div>
         <div className="Img"></div>
-        <div className="PSelect"></div>
+        <div className="PSelect">
+          <i
+            className={`fa fa-camera icon ${selectedIcon === "camera" ? "selected" : ""}`}
+            onClick={() => handleSelect("camera")}
+          ></i>
+          <i
+            className={`fa fa-image icon ${selectedIcon === "image" ? "selected" : ""}`}
+            onClick={() => handleSelect("image")}
+          ></i>
+        </div>
         <div className="IName">
           <label htmlFor="IName">Item Name</label>
           <input type="text" id="IName" className="NameField"></input>
@@ -18,7 +33,10 @@ function FoundPage({ supabase }) {
           <input type="text" id="ILocation" className="FoundField"></input>
         </div>
         <div className="PhoneNumber">
-          <label htmlFor="INumber">Phone Number</label>
+          <div className="PhoneNumberText">
+            <label htmlFor="INumber">Phone Number</label>
+            <i className="fa fa-question-circle"></i>
+          </div>
           <input type="number" id="INumber" className="NumberField"></input>
         </div>
         <textarea
