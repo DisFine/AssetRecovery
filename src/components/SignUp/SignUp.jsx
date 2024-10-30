02be18b47dd49adb4c6377e068c244e7bfdabf41
Daithoulung <daithoulungrongmai@gmail.com>
2024-10-31 00:46:00 +0530

    updated design

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "../SignIn/SignInstyles.css";
+import "../style.css";
 import { useNavigate } from "react-router-dom";
 
 async function signup_function(
--- src/components/lostItems/LostItems.jsx
+++ src/components/lostItems/LostItems.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./style.css";
+import "../style.css";
 
 async function fetchLostItems(supabase, setItems) {
   let { data: Lost_Items, error } = await supabase
