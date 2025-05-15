e8f189cfa0b1706a18e2a91f6b3b127742660e29
Daithoulung <daithoulungrongmai@gmail.com>
2025-05-16 00:42:09 +0530

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,16 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered:", registration);
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  });
+}
